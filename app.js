a1983bcfe4adb4ae035682ff8839691a0d550bcd
drshare <hjyspace@163.com>
2021-10-11 20:14:18 +0800

    fix: 提交

---
+++ app.js
@@ -0,0 +1,42 @@
+const util = require('util');
+// app.js
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+app.use(express.static(path.join(__dirname, './', 'public')));
+
+const PORT = 9528;
+server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+
+
+// socket
+const ws = require('nodejs-websocket');
+const SOCKET_SERVER_PORT = 9527;
+
+const socketServer = ws.createServer(connect => {
+    console.log(`用户连接成功！`);
+    connect.on('text', data => {
+        broadcast(data);
+    })
+
+    connect.on('close', () => {
+        console.log('断开连接1');
+    })
+
+    //注册error事件,用户端口后就会触发该异常
+    connect.on('error',()=>{
+        console.log('用户连接异常');
+    });
+})
+socketServer.listen(SOCKET_SERVER_PORT, () => {
+    console.log(`socket 服务启动监听端口${SOCKET_SERVER_PORT}`);
+});
+
+//广播
+const broadcast = (msg) => {
+    socketServer.connections.forEach(item => {
+        //遍历出每个用户，挨个发消息
+        item.send(msg);
+    });
+}
---
+++ public/js/index.js
@@ -0,0 +1,50 @@
+window.onload = function () {
+    const content = document.getElementById('talk-list')
+    const input = document.getElementById('input')
+    const btn = document.getElementById('btn')
+
+    // 创建WebSocket 连接
+    const socket = new WebSocket('ws://localhost:9527')
+    socket.addEventListener('open', () => {
+        addMessage('连接成功！');
+    })
+
+
+    socket.addEventListener('message', (msg) => {
+        console.log(`来消息了：${msg}`);
+        addMessage(msg.data);
+    })
+
+    socket.addEventListener('close', () => {
+        addMessage('断开链接！');
+    })
+    btn.onclick = sendMessage;
+    input.onkeydown = function (event) {
+        enterSend(event);
+    }
+
+    function sendMessage() {
+        const value = input.value;
+        if (!value) {
+            return;
+        }
+        console.log(`发送消息: ${value}`);
+        socket.send(value);
+        input.value = '';
+    }
+
+    function addMessage(text) {
+        const liNode = document.createElement('li');
+        const textNode = document.createTextNode(text);
+        liNode.appendChild(textNode);
+        content.appendChild(liNode);
+    }
+
+
+    function enterSend(event) {
+        let code = event.keyCode;
+        if (code === 13) {
+            sendMessage();
+        }
+    }
+}
